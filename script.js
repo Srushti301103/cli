4860b02ec55a5195e985d557880ee0d738b5b05b
Srushti301103 <140685475+Srushti301103@users.noreply.github.com>
2024-12-17 19:39:04 +0530

    Add files via upload
    git init
    git add .
    git commit -m "Initial commit"
    git branch -M main
    git remote add origin https://github.com/<your-username>/<repo-name>.git
    git push -u origin main

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const ctaButton = document.querySelector('.btn');
+    ctaButton.addEventListener('click', () => {
+      alert('Thanks for exploring WayReach!');
+    });
+  });
+  
